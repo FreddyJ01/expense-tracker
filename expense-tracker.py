20d8ad9178672b56ab64f72368cebb400be9d1e2
FreddyJ01 <fahrodej01@gmail.com>
2025-01-25 16:08:28 -0600

    Basic Changes to main function

--- expense-tracker.py
+++ expense-tracker.py
@@ -3,3 +3,22 @@ def add_expense(expenses, amount, category):
 
 def main():
     expenses = []
+    while True:
+        print('\nPlease Select One Of The Following Options:')
+        print('1. Add An Expense')
+        print('2. Display All Expenses')
+        print('3. Display Expenses By Category')
+        print('4. Display Total Of Expenses')
+        print('5. Exit')
+        user_choice = input('Make Selection: ')
+
+        if user_choice == '1':
+            pass
+        elif user_choice == '2':
+            pass
+        elif user_choice == '3':
+            pass
+        elif user_choice == '4':
+            pass
+        elif user_choice == '5':
+            pass
